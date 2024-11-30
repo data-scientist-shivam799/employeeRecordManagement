438fe1eee1cbf3edd1f556dcecc87c8332fa02e7
Shivam Kumar <shivam799kumar@gmail.com>
2024-11-30 19:36:28 +0530

    added user page

--- employee/urls.py
+++ employee/urls.py
@@ -5,4 +5,8 @@ from employee import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('registration', views.registration, name='registration'),
+    path('emp_login', views.emp_login, name='emp_login'),
+    path('emp_home', views.emp_home, name='emp_home'),
+    path('profile', views.profile, name='profile'),
+    path('logout', views.Logout, name='logout'),
 ]
--- employee/views.py
+++ employee/views.py
@@ -1,8 +1,92 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import *
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
 
+def emp_login(request):
+    error = ""
+    if request.method == "POST":
+        u = request.POST['emailid']
+        p = request.POST['password']
+
+        user = authenticate(username = u, password = p)
+
+        if user:
+            login(request, user)
+            error = "no"
+        else:
+            error = "yes"
+
+    return render(request, 'emp_login.html', locals())
+
+def emp_home(request):
+    if not request.user.is_authenticated:
+        return redirect('emp_login')
+    
+    return render(request, 'emp_home.html')
+
+def Logout(request):
+    logout(request)
+    return redirect('index')
+
 def registration(request):
-    return render(request, 'registration.html')
+    error = ""
+    if request.method == "POST":
+        fn = request.POST['firstname']
+        ln = request.POST['lastname']
+        ec = request.POST['empcode']
+        em = request.POST['email']
+        pwd = request.POST['pwd']
+
+        try:
+            user = User.objects.create_user(first_name = fn, last_name = ln, username = em, password = pwd)
+            EmployeeDetail.objects.create(user = user, empcode = ec)
+            error = "no"
+
+        except:
+            error = "yes"
+
+    return render(request, 'registration.html', locals())
+
+def profile(request):
+    if not request.user.is_authenticated:
+        return redirect('emp_login')
+    
+    error = ""
+    user = request.user
+    employee = EmployeeDetail.objects.get(user = user)
+
+    if request.method == "POST":
+        fn = request.POST['firstname']
+        ln = request.POST['lastname']
+        ec = request.POST['empcode']
+        dept = request.POST['department']
+        designation = request.POST['designation']
+        contact = request.POST['contact']
+        jdate = request.POST['jdate']
+        gender = request.POST['gender']
+
+        employee.user.first_name = fn
+        employee.user.last_name = ln
+        employee.empcode = ec
+        employee.empdept = dept
+        employee.designation = designation
+        employee.contact = contact
+        employee.gender = gender
+
+        if jdate:
+            employee.joiningdate = jdate
+
+
+        try:
+            employee.save()
+            employee.user.save()
+            error = "no"
+
+        except:
+            error = "yes"
+
+    return render(request, 'profile.html', locals())
